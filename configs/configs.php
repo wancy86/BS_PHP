e61c22e1b3898b8afc293b1d2bae8ad27485d554
wancy86 <wancy86@sina.com>
2016-01-19 23:17:54 +0800

    db change

--- configs/configs.php
+++ configs/configs.php
@@ -2,5 +2,5 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PWD","111222");
-define("DB_DBNAME","shopImooc");
+define("DB_DBNAME","BoyStyle");
 define("DB_CHARSET","utf8");
