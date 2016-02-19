76dcb353b9fadcf56c3f83b7503c1d7336edd6f0
Mark <wancy86@sina.com>
2016-02-19 21:28:33 +0800

    table

--- database.sql
+++ database.sql
@@ -1,12 +1,17 @@
 use boystyle;
 
+/*drop TABLE BS_ProURL;*/
+
 create TABLE IF NOT EXISTS BS_ProURL(
-	id int AUTO_INCREMENT PRIMARY,
-	url varchar(500) CHARACTER SET UTF8,
-	title varchar(200)CHARACTER SET UTF8,
-	img_url varchar(300)CHARACTER SET UTF8,
-	img_list varchar(500)CHARACTER SET UTF8,
+	id int AUTO_INCREMENT,
+	url varchar(500),
+	title varchar(200),
+	img_url varchar(300),
+	img_list varchar(500),
 	show_order int default 0,
-	category varchar(50) CHARACTER SET UTF8,
-	entrydate date default now()
-);
+	category varchar(50),
+	entrydate timestamp  default CURRENT_TIMESTAMP,
+	primary key(id)
+)default charset=utf8;
+
+select * from BS_ProURL;
