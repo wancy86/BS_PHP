99fb81ada91336b9c3101d6d23483b9b561db801
Mark <wancy86@sina.com>
2016-03-01 21:16:48 +0800

    !input_source!

--- login.php
+++ login.php
@@ -1,74 +1,80 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>登陆</title>
-<link type="text/css" rel="stylesheet" href="styles/reset.css">
-<link type="text/css" rel="stylesheet" href="styles/main.css">
-<!--[if IE 6]>
-<script type="text/javascript" src="js/DD_belatedPNG_0.0.8a-min.js"></script>
-<script type="text/javascript" src="js/ie6Fixpng.js"></script>
-<![endif]-->
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body>
-	<div class="headerBar">
-		<div class="logoBar login_logo">
-			<div class="comWidth">
-				<div class="logo fl">
-					<a href="#"><img src="images/logo.jpg" alt="慕课网"></a>
+    <title>登录</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <div class="container-fluid">
+		<div class="row" style="margin-bottom:100px;">
+			<div class="col-md-12">
+				<!--navbar-->
+				<?php require_once 'header.php';?>
+
+				<!--content-->
+				<div class="row">
+					<div class="col-md-3">
+					</div>
+					<div class="col-md-6">
+						<form role="form" class="form-horizontal">
+							<div class="form-group">
+								<label class="col-sm-2 control-label" for="emailcell">账号 : </label>
+								<div class="col-sm-4">
+									<input type="email" class="form-control" id="emailcell" placeholder="用户名/邮箱"/>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-2 control-label" for="pwd">密码 : </label>
+								<div class="col-sm-4">
+									<input type="password" class="form-control" id="pwd" placeholder="密码必须是6-25位数字、字母、符号"/>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-2 control-label" for="emailcell">验证码 : </label>
+								<div class="col-sm-4">
+									<input type="email" class="form-control" id="emailcell" placeholder="用户名/邮箱"/>
+								</div>
+							</div>
+							<div class="form-group">
+							    <div class="col-sm-offset-2 col-sm-10">
+							      <div class="checkbox">
+							        <label>
+							          <input type="checkbox"> 30天自动登录
+							        </label>
+							      </div>
+							    </div>
+							</div>
+							<div class="form-group">
+							    <div class="col-sm-offset-2 col-sm-10">
+							      <button type="submit" class="btn btn-success">登录</button>
+							    </div>
+							</div>
+						</form>
+
+					</div>
+					<div class="col-md-3">
+					</div>
 				</div>
-				<h3 class="welcome_title">欢迎登陆</h3>
 			</div>
 		</div>
+
+		<!--footer-->
+		<?php require_once 'footer.php';?>
 	</div>
 
-	<div class="loginBox">
-		<div class="login_cont">
-			<form method="post" action="doAction.php?act=login">
-				<ul class="login">
-					<li class="l_tit">邮箱/用户名/手机号</li>
-					<li class="mb_10"><input type="text" name="username"placeholder="请输入用户名" class="login_input user_icon"></li>
-					<li class="l_tit">密码</li>
-					<li class="mb_10"><input type="password" name="password"class="login_input user_icon"></li>
-					<li class="autoLogin"><input type="checkbox" id="a1"class="checked"><label for="a1">自动登陆</label></li>
-					<li><input type="submit" value="" class="login_btn"></li>
-				</ul>
-			</form>
-			<div class="login_partners">
-				<p class="l_tit">使用合作方账号登陆网站</p>
-				<ul class="login_list clearfix">
-					<li><a href="#">QQ</a></li>
-					<li><span>|</span></li>
-					<li><a href="#">网易</a></li>
-					<li><span>|</span></li>
-					<li><a href="#">新浪微博</a></li>
-					<li><span>|</span></li>
-					<li><a href="#">腾讯微薄</a></li>
-					<li><span>|</span></li>
-					<li><a href="#">新浪微博</a></li>
-					<li><span>|</span></li>
-					<li><a href="#">腾讯微薄</a></li>
-				</ul>
-			</div>
-		</div>
-		<a class="reg_link" href="#"></a>
-	</div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 
-	<div class="hr_25"></div>
-	<div class="footer">
-		<p>
-			<a href="#">慕课简介</a><i>|</i><a href="#">慕课公告</a><i>|</i> <a href="#">招纳贤士</a><i>|</i><a
-				href="#">联系我们</a><i>|</i>客服热线：400-675-1234
-		</p>
-		<p>Copyright &copy; 2006 - 2014
-			慕课版权所有&nbsp;&nbsp;&nbsp;京ICP备09037834号&nbsp;&nbsp;&nbsp;京ICP证B1034-8373号&nbsp;&nbsp;&nbsp;某市公安局XX分局备案编号：123456789123</p>
-		<p class="web">
-			<a href="#"><img src="images/webLogo.jpg" alt="logo"></a><a href="#"><img
-				src="images/webLogo.jpg" alt="logo"></a><a href="#"><img
-				src="images/webLogo.jpg" alt="logo"></a><a href="#"><img
-				src="images/webLogo.jpg" alt="logo"></a>
-		</p>
-	</div>
-</body>
+  </body>
 </html>
