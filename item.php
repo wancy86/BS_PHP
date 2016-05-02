5687e2a73f6b8176594194f4201a804dd97ddba8
wancy86 <wancy86@sina.com>
2016-05-02 22:42:10 +0800

    sss

--- item.php
+++ item.php
@@ -1,10 +1,26 @@
 <?php
+header("Content-type: text/html; charset=utf-8");
 require_once 'lib/mysql.func.php';
 
-$pro_id = $_GET['pid'];
+// http://localhost/boystyle/item.php?pro_id=43879806728
+$pro_id = $_GET['pro_id'];
 // echo $pro_id;
+$query = " select pro_id ,title ,img_url ,detail_url ,shop_name ,price ,month_sold ,comm_percent ,seller_ww ,back_BB ,";
+$query .= " short_tbk_url ,tbk_url ,commission ,earn ,img_list ,show_order ,P.cat_id ,entrydate ,disabled ";
+$query .= " from BS_ProInfo AS P";
+$query .= " join BS_Category as C on P.cat_id=C.cat_id";
+$query .= " where P.pro_id=$pro_id";
+
+$result = mysqli_query(connect(), $query);
+// var_dump($result);
+// echo "<hr>";
+$item = '';
+if ($result) {
+	$item = mysqli_fetch_assoc($result);
+}
 ?>
     <html>
+
     <head>
         <title>BoyStyle</title>
         <?php require_once 'style.php';?>
@@ -17,16 +33,24 @@ $pro_id = $_GET['pid'];
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <div class="thumbnail">
-                                <a class="pro" href="javascript:void(0);" pro_id="43879806728" url="http://s.click.taobao.com/t?e=m%3D2%26s%3DH5Z1C22R4PscQipKwQzePOeEDrYVVa64LKpWJ%2Bin0XLjf2vlNIV67hWF63xfljJaoAgJVlbS%2FO9Og2r8NdThQAD%2F0NtdZ9kyH9QiDT32gnJJPVbpKpZHQey%2FMwu8z29MQ%2FmkFCs8mWpbDveG3Yq1UMYMXU3NNCg%2F&amp;pvid=12_61.141.137.74_6271_1456553661065" target="_blank"><img alt="包邮春夏新款女装高腰短裙裤半身裙韩版蓬蓬裙大摆红色伞裙防走光" src="http://img01.taobaocdn.com/bao/uploaded/i1/1826393730/TB2jklakpXXXXXvXXXXXXXXXXXX_!!1826393730.jpg"></a>
+                                <?php if ($result) {?>
+                                <a class="pro" href="javascript:void(0);" pro_id="<?php echo $item['pro_id']; ?>" url="<?php echo $item['detail_url']; ?>" target="_blank"><img alt="<?php echo $item['detail_url']; ?>" src="<?php echo $item['img_url']; ?>"></a>
                                 <div class="caption">
-                                    <h3>包邮春夏新款女装高腰短裙裤半身裙韩版蓬蓬裙大摆红色伞裙防走光</h3>
-                                    <p> 价格: ￥58 / 返利: 5.8 BB / 月销量:118 </p>
-                                    <p> <a class="btn btn-danger pro" href="javascript:void(0);" pro_id="43879806728" url="http://s.click.taobao.com/t?e=m%3D2%26s%3DH5Z1C22R4PscQipKwQzePOeEDrYVVa64LKpWJ%2Bin0XLjf2vlNIV67hWF63xfljJaoAgJVlbS%2FO9Og2r8NdThQAD%2F0NtdZ9kyH9QiDT32gnJJPVbpKpZHQey%2FMwu8z29MQ%2FmkFCs8mWpbDveG3Yq1UMYMXU3NNCg%2F&amp;pvid=12_61.141.137.74_6271_1456553661065" target="_blank">去看看</a>
-                                        <button tabindex="0" onclick="AddFavorite(this, 43879806728)" class="btn"><span class="glyphicon glyphicon-heart-empty"></span> 添加收藏</button>
+                                    <h3><?php echo $item['title']; ?></h3>
+                                    <p> 价格: ￥
+                                        <?php echo $item['price'] ?> / 返利:
+                                        <?php echo $item['back_BB'] ?> BB / 月销量:
+                                        <?php echo $item['month_sold'] ?> </p>
+                                    <p> <a class="btn btn-danger pro" href="javascript:void(0);" pro_id="<?php echo $item['pro_id']; ?>" url="<?php echo $item['detail_url']; ?>" target="_blank">去看看</a>
+                                        <button tabindex="0" onclick="AddFavorite(this, <?php echo $item['pro_id']; ?>)" class="btn"><span class="glyphicon glyphicon-heart-empty"></span> 添加收藏</button>
                                     </p>
                                 </div>
+                                <?php } else {?>
+                                <?php header("location:http://localhost/boystyle/index.php");?>
+                                <?php }?>
                             </div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
