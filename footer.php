020a16e716e88e8b5f3202d0d7db3c450bc4205e
wancy86 <wancy86@sina.com>
2016-05-25 23:17:23 +0800

    footer

--- footer.php
+++ footer.php
@@ -1,63 +1,54 @@
 <div class="row">
     <div class="col-md-12">
         <hr style="margin-top:30px;">
-        <div class="col-md-3">
+        <div class="col-md-3 col-md-offset-3">
             <dl>
                 <dt>
-                    Description lists
+                    为什么要用BoyStyle
                 </dt>
                 <dd>
-                    A description list is perfect for defining terms.
-                </dd>
-                <dt>
-                    Euismod
-                </dt>
-                <dd>
-                    Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
+                    同一商品，从我们这里点击购买，有折扣！
                 </dd>
                 <dd>
-                    Donec id elit non mi porta gravida at eget metus.
+                    专属链接邀请好友注册，他人购物，你拿佣金！
+                </dd>
+                <dd>
+                    所有宝贝，件件精选，最美的，最好的，都在这里！
+                </dd>
+                <dd>
+                    逛淘宝的同时，培养你的第二职业！
+                </dd>
+                <dd>
+                    突然有一天你发现，你拿到的佣金比你的工资还高，还上什么班呢?
                 </dd>
             </dl>
         </div>
-        <div class="col-md-3">
-            <address>
-                <strong>Twitter, Inc.</strong>
-                <br> 795 Folsom Ave, Suite 600
-                <br> San Francisco, CA 94107
-                <br>
-                <abbr title="Phone">P:</abbr> (123) 456-7890
-            </address>
-        </div>
-        <div class="col-md-3">
-            <ol>
+        
+        <div class="col-md-2">
+            <ul class="list-unstyled">
                 <li>
-                    Lorem ipsum dolor sit amet
+                    <b>网站信息</b>
                 </li>
                 <li>
-                    Consectetur adipiscing elit
+                    <a style="color:#333" href="http://www.miibeian.gov.cn/state/outPortal/loginPortal.action">粤ICP备08109441号</a>
                 </li>
                 <li>
-                    Integer molestie lorem at massa
+                    © 2015-2016 BoyStyle.CN 版权所有
                 </li>
-                <li>
-                    Facilisis in pretium nisl aliquet
-                </li>
-                <li>
-                    Nulla volutpat aliquam velit
-                </li>
-            </ol>
-        </div>
-        <div class="col-md-3">
-            <blockquote>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
-                </p> <small>Someone famous <cite>Source Title</cite></small>
+                <li>                    
                 <script type="text/javascript">
                 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
                 document.write(unescape("%3Cspan id='cnzz_stat_icon_1257892590'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1257892590' type='text/javascript'%3E%3C/script%3E"));
                 </script>
-            </blockquote>
+                </li>
+            </ol>
+        </div>
+        <div class="col-md-1">
+            <address>
+                <strong>联系我们</strong>
+                <br>Tel: (86) 13028865077
+                <br> <a href="mailto:BoyStyle_CN@163.com">BoyStyle_CN@163.com</a> 
+            </address>
         </div>
     </div>
 </div>
